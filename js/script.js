6deab4ff7403f5ea13bab0a10c9080aefc7e2d43
Pavel Pak <talkin.watermelon@gmail.com>
2022-04-05 01:55:14 +0900

    Added tab script

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) { //set default value if we call function without arg
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent(); //on load screen we need default settings
+    showTabContent(); //on load screen we need default settings, default arg value = 0
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target; //if we will use event.target multiple times it is better to set variable
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) { //compare terget tab with tabs NodeList, to get the index = i, in order to put this index for tabContent
+                    hideTabContent(); //first hide all tabs
+                    showTabContent(i); //second display tab by index
+                }
+            });
+        }
+    });
+
+});
