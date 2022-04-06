3324e74f07725332e2ba1678352ba09eb0c63d11
Pavel Pak <talkin.watermelon@gmail.com>
2022-04-06 22:57:43 +0900

    Added timer script in promotion section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
+    /* Tab Content */
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
+
 
     function hideTabContent() {
         tabsContent.forEach(item => {
@@ -34,5 +36,61 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    /* Timer */
+
+    const deadline = '2022-04-11';
+
+    function getTimeRemaining(endtime) { //get difference between current date and arg date
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function getZero(num) { //function to add 0 if digit is less than 10
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); //in order not to wait 1 second when we refresh website and default(in HTML) values change to actual ones
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+    console.log(new Date(Date.parse(deadline)));
+
 
 });
