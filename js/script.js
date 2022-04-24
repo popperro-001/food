4912b05782b416d00cd41387f043d088f6806e6c
Pavel Pak <talkin.watermelon@gmail.com>
2022-04-24 22:27:40 +0900

    Added check for past date

--- js/script.js
+++ js/script.js
@@ -42,11 +42,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2022-05-06';
 
     function getTimeRemaining(endtime) { //get difference between current date and arg date
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-            minutes = Math.floor((t / 1000 / 60) % 60),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        if (t <= 0) { //check for past date
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor((t / (1000 * 60 * 60) % 24));
+            minutes = Math.floor((t / 1000 / 60) % 60);
             seconds = Math.floor((t / 1000) % 60);
+        }
+
 
         return {
             'total': t,
